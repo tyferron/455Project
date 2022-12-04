153ef4968abb1ec5caf87d505133b9f013a2408b
Mindarius <67028450+Mindarius@users.noreply.github.com>
2022-12-04 11:47:07 -0600

    Merge Monkey
    Moved DBConnection setup from app.java to server.java

--- server/src/main/java/csci455/project/chatroom/server/Server.java
+++ server/src/main/java/csci455/project/chatroom/server/Server.java
@@ -2,14 +2,41 @@ package csci455.project.chatroom.server;
 
 import java.io.IOException;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.postgresql.jdbc.PgConnection;
 
 public class Server 
 {
     static int SERVER_PORT = 29000;
+    static PgConnection conn;
     public static void main(String[] args)
     {
     	boolean close=false;
-        //Is an interface needed? or is this just a background process?
+    	try
+        {
+            conn = new PgConnection(null, null, null);
+            Connection c = Environment.getConnection();
+            System.out.println("Connection Sucessfull.");
+            c.close();
+        }
+        catch (Exception ex)
+        {
+            if (ex instanceof ClassNotFoundException)
+            {
+                System.out.println("Unable to find driver.");
+            }
+            else if (ex instanceof SQLException)
+            {
+                System.out.println("Connection Unsuccessfull.");
+            }
+            else
+            {
+                System.out.println(ex.getClass().getName());
+            }
+            ex.printStackTrace();
+        }
         try{
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             System.out.println("Opening server on port: "+SERVER_PORT);
