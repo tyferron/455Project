57bdc738654ec725b869ba0ab472050301cb45a8
Zak Ray Merrigan <zak.merrigan@ndsu.edu>
2022-12-06 00:12:23 -0600

    This is the Database File.

---
+++ database.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE "ChatroomDB"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE TABLE IF NOT EXISTS public."ChatRoom"
+(
+    "RoomID" integer NOT NULL DEFAULT nextval('"ChatRoom_RoomID_seq"'::regclass),
+    "RoomName" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    "Password" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    "MessageHistory" character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "ChatRoom_pkey" PRIMARY KEY ("RoomID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."ChatRoom"
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public."Users"
+(
+    "UserID" integer NOT NULL DEFAULT nextval('"Users_UserID_seq"'::regclass),
+    "UserName" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    "Password" character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Pk_Users" PRIMARY KEY ("UserID")
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Users"
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public."UserToChatrooms"
+(
+    "UserID" integer NOT NULL,
+    "RoomID" integer NOT NULL,
+    CONSTRAINT "Pk_combined" PRIMARY KEY ("UserID", "RoomID"),
+    CONSTRAINT "FK_ChatRoom" FOREIGN KEY ("RoomID")
+        REFERENCES public."ChatRoom" ("RoomID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "FK_User" FOREIGN KEY ("UserID")
+        REFERENCES public."Users" ("UserID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."UserToChatrooms"
+    OWNER to postgres;
